4c3418a374c91936ad3136fdb6c533d266bfd9ea
zhaqyy <zaqrashyy7@gmail.com>
2024-07-31 08:34:57 +0100

    ccdik added, model reboned

--- src/arm.jsx
+++ src/arm.jsx
@@ -1,35 +1,82 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { Instance, Instances, useGLTF } from "@react-three/drei";
-import { Euler, Vector3 } from "three";
+import { useFrame, useGraph } from "@react-three/fiber";
+import * as SkeletonUtils from "three/examples/jsm/utils/SkeletonUtils";
+
+import { CCDIKSolver } from 'three/addons/animation/CCDIKSolver.js';
 
 
-function ArmInstance({ random, position, scale }) {
-    const ref = useRef();
-  
-    useEffect(() => {
-      if (ref.current) {
-        ref.current.lookAt(0, 0, 0); // Make the arm look at the center (0, 0, 0)
+function ArmInstance({ position, initialScale, model }) {
+  const ref = useRef();
+  const clonedModel = useRef(SkeletonUtils.clone(model));
+  const armRotation = [Math.PI / 4, Math.PI / 2, 0]; // Rotating on the Y-axis
+  const solver = useRef();
+
+  useEffect(() => {
+    if (clonedModel.current) {
+      clonedModel.current.lookAt(0, 0, 0); // Make the arm look at the center (0, 0, 0)
+      // console.log('rotation:', clonedModel.current.rotation);
+
+      // Setup CCDIKSolver
+      const bones = [];
+      clonedModel.current.traverse((object) => {
+        if (object.isBone) bones.push(object);
+      });
+      const skinnedMesh = clonedModel.current.children[0].children[0];
+      const iks = [
+        {
+          target: 3,
+          effector: 2,
+          links: [ { index: 1 } ] ,
+        },
+      ];
+      solver.current = new CCDIKSolver(skinnedMesh, iks);
+      // console.log(bones);
+    }
+  }, []);
+
+    useFrame((state) => {
+    const elapsedTime = state.clock.getElapsedTime();
+    const stretchFactor = elapsedTime * 0.1; // Adjust the stretch factor as needed
+
+    if (clonedModel.current) {
+      const skinnedMesh = clonedModel.current.children[0].children[0];
+      const boneToScale =clonedModel.current.children[0].children[1]; // Example: Scaling the second bone in the skeleton
+      // boneToScale.scale.y = stretchFactor;
+
+      // // Check position to stop animation
+      // const position = new THREE.Vector3();
+      // skinnedMesh.getWorldPosition(position);
+      // if (position.distanceTo(new THREE.Vector3(0, 0, 0)) < 0.1) {
+      //   boneToScale.scale.y = 1; // Reset scale when near center
+      // }
+
+      // console.log(boneToScale);
+      // Update IK solver
+      if (solver.current) {
+        solver.current.update();
       }
-    }, []);
-  
-    return (
-      <group position={position} scale={scale}>
-        <Instance ref={ref} />
-      </group>
-    );
-  }
+    }
+  });
 
-  
-export function Arm({ datas, position, rotation, scale }) {
-  const { nodes } = useGLTF("/arms.glb");
-
-return (
-    <Instances range={100} material={nodes.LArm.material} geometry={nodes.LArm.geometry} >
-      {datas.map((props, i) => (
-        <ArmInstance key={i} {...props} />
-      ))}
-    </Instances>
+  return (
+    <group rotation={armRotation} position={position} scale={initialScale}>
+      <primitive object={clonedModel.current} />
+    </group>
   );
 }
 
-useGLTF.preload("/arms.glb");
+export function Arm({ datas }) {
+  const { scene } = useGLTF('/armB.glb');
+  const model = useMemo(() => SkeletonUtils.clone(scene), [scene]);
+  return (
+    <group>
+      {datas.map((props, i) => (
+        <ArmInstance key={i} {...props} model={model} />
+      ))}
+    </group>
+  );
+}
+
+
+useGLTF.preload("/armB.glb");
